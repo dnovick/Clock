0d58c6c180668cc36bbd4e6684434bab1b8349d5
Dave Novick <davenovick@hotmail.com>
2017-08-02 10:28:44 -0700

    Initial commit of Clock application.

---
+++ Clock/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ctxMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.miTransparent = new System.Windows.Forms.ToolStripMenuItem();
+            this.miFullSize = new System.Windows.Forms.ToolStripMenuItem();
+            this.miMove = new System.Windows.Forms.ToolStripMenuItem();
+            this.miExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.miHide = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.ctxMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ctxMenu
+            // 
+            this.ctxMenu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ctxMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.ctxMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miHide,
+            this.miFullSize,
+            this.miMove,
+            this.miTransparent,
+            this.miExit});
+            this.ctxMenu.Name = "ctxMenu";
+            this.ctxMenu.ShowCheckMargin = true;
+            this.ctxMenu.ShowImageMargin = false;
+            this.ctxMenu.Size = new System.Drawing.Size(251, 187);
+            // 
+            // miTransparent
+            // 
+            this.miTransparent.CheckOnClick = true;
+            this.miTransparent.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.miTransparent.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.miTransparent.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.miTransparent.Name = "miTransparent";
+            this.miTransparent.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.T)));
+            this.miTransparent.Size = new System.Drawing.Size(250, 30);
+            this.miTransparent.Text = "Transparent";
+            this.miTransparent.TextImageRelation = System.Windows.Forms.TextImageRelation.Overlay;
+            this.miTransparent.Click += new System.EventHandler(this.miTransparent_Click);
+            // 
+            // miFullSize
+            // 
+            this.miFullSize.CheckOnClick = true;
+            this.miFullSize.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.miFullSize.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.miFullSize.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.miFullSize.Name = "miFullSize";
+            this.miFullSize.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
+            this.miFullSize.Size = new System.Drawing.Size(250, 30);
+            this.miFullSize.Text = "Full Size";
+            this.miFullSize.TextImageRelation = System.Windows.Forms.TextImageRelation.Overlay;
+            this.miFullSize.Click += new System.EventHandler(this.miFullSize_Click);
+            // 
+            // miMove
+            // 
+            this.miMove.CheckOnClick = true;
+            this.miMove.Name = "miMove";
+            this.miMove.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.M)));
+            this.miMove.Size = new System.Drawing.Size(250, 30);
+            this.miMove.Text = "Move";
+            this.miMove.Click += new System.EventHandler(this.miMove_Click);
+            // 
+            // miExit
+            // 
+            this.miExit.Name = "miExit";
+            this.miExit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.miExit.Size = new System.Drawing.Size(250, 30);
+            this.miExit.Text = "Exit";
+            this.miExit.Click += new System.EventHandler(this.miExit_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.ContextMenuStrip = this.ctxMenu;
+            this.lblTime.Font = new System.Drawing.Font("Courier New", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.White;
+            this.lblTime.Location = new System.Drawing.Point(12, 14);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(59, 60);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.Text = "t";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // miHide
+            // 
+            this.miHide.Name = "miHide";
+            this.miHide.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.H)));
+            this.miHide.Size = new System.Drawing.Size(250, 30);
+            this.miHide.Text = "Hide";
+            this.miHide.Click += new System.EventHandler(this.miHide_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(473, 70);
+            this.ContextMenuStrip = this.ctxMenu;
+            this.ControlBox = false;
+            this.Controls.Add(this.lblTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ctxMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip ctxMenu;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.ToolStripMenuItem miTransparent;
+        private System.Windows.Forms.ToolStripMenuItem miFullSize;
+        private System.Windows.Forms.ToolStripMenuItem miExit;
+        private System.Windows.Forms.ToolStripMenuItem miMove;
+        private System.Windows.Forms.ToolStripMenuItem miHide;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ Clock/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Form1 : Form
+    {
+
+        Timer _timer;
+
+        //These are the borderless dimensions.
+        int _initWidth;
+        int _initHeight;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            _timer = new Timer();
+            _timer.Interval = 10;
+            _timer.Tick += _timer_Tick;
+            _timer.Start();
+
+            //There appears to be a bug in the VS 2017 designer (might be in older versions too).
+            //Even though the designer has been set for RightToLeft.No, the menu still displays right to left.
+            //Therefore we explicitly set it on load.
+            //ctxMenu.RightToLeft = RightToLeft.No;
+            _initWidth = this.Width;
+            _initHeight = this.Height;
+
+         
+
+        }
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            int hours = DateTime.Now.TimeOfDay.Hours;
+            if (hours > 12)
+            {
+
+                hours -= 12;
+            }
+
+            int minutes = DateTime.Now.TimeOfDay.Minutes;
+            int seconds = DateTime.Now.TimeOfDay.Seconds;
+
+            int ms = DateTime.Now.TimeOfDay.Milliseconds;
+
+            string strTime = String.Format("{0}:{1}:{2}:{3}", hours.ToString().PadLeft(2, '0'), minutes.ToString().PadLeft(2, '0'), seconds.ToString().PadLeft(2, '0'), ms.ToString().PadLeft(3, '0'));
+
+            lblTime.Text = strTime;
+
+
+
+        }
+
+
+        private void miTransparent_Click(object sender, EventArgs e)
+        {
+            //miTransparent.Checked = !miTransparent.Checked;
+
+
+
+            if (miTransparent.Checked)
+            {
+                this.Opacity = .30;
+            }
+            else
+            {
+                this.Opacity = 1.0;
+            }
+
+        }
+
+        protected void centerToScreen()
+        {
+            Screen screen = Screen.FromControl(this);
+
+            Rectangle workingArea = screen.WorkingArea;
+            this.Location = new Point()
+            {
+                X = Math.Max(workingArea.X, workingArea.X + (workingArea.Width - this.Width) / 2),
+                Y = Math.Max(workingArea.Y, workingArea.Y + (workingArea.Height - this.Height) / 2)
+            };
+        }
+
+        private void resizeClock(bool full)
+        {
+            Screen s = Screen.FromControl(this);
+
+            if (full)
+            {
+                this.Width = 2500;
+                this.Height = 400;
+                              
+
+                lblTime.Font = new Font(lblTime.Font.FontFamily, 250);
+
+               
+            }
+            else
+            {
+                this.Width = _initWidth;
+                this.Height = _initHeight;
+
+
+                lblTime.Font = new Font(lblTime.Font.FontFamily, 28);
+
+            }
+
+            centerToScreen();
+        }
+
+        private void miFullSize_Click(object sender, EventArgs e)
+        {
+
+            //miFullSize.Checked = !miFullSize.Checked;
+
+            resizeClock(miFullSize.Checked);
+
+
+
+        }
+
+
+        private void miMove_Click(object sender, EventArgs e)
+        {
+            if (miMove.Checked)
+            {
+                this.FormBorderStyle = FormBorderStyle.FixedSingle;
+                
+            }
+            else
+            {
+                this.FormBorderStyle = FormBorderStyle.None;
+
+            }
+        }
+
+        private void miExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void miHide_Click(object sender, EventArgs e)
+        {
+
+            this.WindowState = FormWindowState.Minimized;
+          
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon.Visible = true;
+
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+        }
+    }
+}
